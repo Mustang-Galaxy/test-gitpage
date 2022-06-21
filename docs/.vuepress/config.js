6b29666b458b7c89baed87916e5f70b2f3e6d700
shawnwang <39792324+Mustang-Galaxy@users.noreply.github.com>
2022-06-21 21:11:34 +0800

    theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,8 @@
-const { defaultTheme } = require('@vuepress/theme-default')
-
 module.exports = {
   title: 'hello vue press',
   description: 'just playing around',
   base: '/test-gitpage/',
-  theme: defaultTheme({
-    sidebar: [
-      {
-        text: 'Foo',
-        link: '/foo/',
-        children: [
-          {
-            text: 'github',
-            link: 'https://github.com',
-            children: [],
-          },
-        ],
-      },
-      '/guide/configure.md',
-    ],
-  }),
+  themeConfig: {
+    sidebar: ['/', '/page-a', ['/page-b', 'Explicit link text']],
+  },
 }
